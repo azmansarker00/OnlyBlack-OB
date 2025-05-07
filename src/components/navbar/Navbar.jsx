23b0666d7846482f41505afd682dad544f9d3cc9
Azman Sarker <azmansarker861@gmail.com>
2025-05-07 23:31:40 +0600

    updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -106,13 +106,13 @@ const Navbar = () => {
                 Shop: (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-4 h-4 mr-1"
+                    className="w-4 h-4 mr-1 inline"
                     viewBox="0 0 24 24"
                     fill="none"
-                    stroke="#9CA3AF"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
                     <path d="M3 9L1 6h22l-2 3" />
                     <path d="M1 6h22v2a4 4 0 0 1-4 4H5a4 4 0 0 1-4-4V6z" />
@@ -274,13 +274,13 @@ const Navbar = () => {
                 Shop: (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-4 h-4 mr-1"
+                    className="w-4 h-4 mr-1 inline"
                     viewBox="0 0 24 24"
                     fill="none"
-                    stroke="#9CA3AF"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   >
                     <path d="M3 9L1 6h22l-2 3" />
                     <path d="M1 6h22v2a4 4 0 0 1-4 4H5a4 4 0 0 1-4-4V6z" />
