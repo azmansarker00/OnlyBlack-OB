87799373ce8477ba6dca9a68b402cc6cf43d6bb8
Azman Sarker <azmansarker861@gmail.com>
2025-05-13 21:16:26 +0600

    updated

--- src/pages/Admin/productPages/Addproduct.jsx
+++ src/pages/Admin/productPages/Addproduct.jsx
@@ -13,6 +13,12 @@ function AddProduct() {
     imageUrl: "",
     category: "",
     description: "",
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
   });
 
   return (
